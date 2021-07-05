e045bdf31536374515b39fb49d70d0a44b977c58
hyolyn <99hyolyn@gmail.com>
2021-07-05 18:09:56 +0900

    210705

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -1,1 +1,37 @@
 'use strict';
+
+
+// digit btns 클릭시 숫자가 dispaly에 출력
+function digitBtnHandler(){
+    const digits = document.querySelectorAll('.digit');
+    digits.forEach( digit=> { digit.addEventListener('click', (evt)=>{
+        const display= document.querySelector('.display');
+        let targetDigit = evt.target.innerText;
+        display.value +=targetDigit;
+    })
+})
+};
+
+
+// 연산버튼 클릭시
+function calculate(operator, val1, val2){
+    if(operator === '+'){
+        return val1 + val2;
+    }else if(operator === '-'){
+        return val1 - val2;
+    }else if(operator === '*'){
+        return val1 * val2;
+    }else if(operator === '/'){
+        return val1 / val2;
+    }
+}
+
+
+function operatorBtnHandler(){
+    const operatorBtns = document.querySelectorAll('.operator');
+     operatorBtns.forEach( operatorBtn => {
+        operatorBtns.addEventListener('click', (evt)=> {
+            const display= document.querySelector('.display');
+            let displayVal= Number(dispaly)
+     })})
+}
