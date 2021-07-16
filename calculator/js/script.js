dfeba7966f80c0ee01129e1eabcbb14230d0abcf
hyolyn <99hyolyn@gmail.com>
2021-07-16 18:56:56 +0900

    210716

--- calculator/js/script.js
+++ calculator/js/script.js
@@ -1,66 +1,28 @@
 'use strict';
 
 
-let currentOp = '',
-    currentVal = 0;
+//1 숫자 배열을 변수에 저장, 클릭시 배열 각각에 클릭이벤트, 숫자 display
+//2. 연산자함수
+//3. +, -, *, /, = 클릭시 display빈화면 출력 , 연산자 함수 이용하여 계산, = 버튼 클릭시 계산 결과 보여줌
+//4. clear btn 클릭 시 display 리셋
 
 
-// digit btns 클릭시 숫자가 dispaly에 출력
+let currentOp='',
+    currentVal=0;
+
+
 function digitBtnHandler(){
-    const digits = document.querySelectorAll('.digit');
-    digits.forEach( digit=> { digit.addEventListener('click', (evt)=>{
-        const display= document.querySelector('.display');
-        let targetDigit = evt.target.innerText;
-        display.value +=targetDigit;
-    })
-})
+    const digits= document.querySelectorAll('.digit');
+    digitBtnHandler.forEach(digit => {
+        digit.addEventListener('click', (evt) => {
+            const display=document.querySelector('#input_box');
+            let targetDigit =evt.target.innerText;
+            display.value += targetDigit;
+        });
+    });
 };
 
 
-//연산해주는 함수
-function calculate(operator, val1, val2){
-    if(operator === '+'){
-        return val1 + val2;
-    }else if(operator === '-'){
-        return val1 - val2;
-    }else if(operator === '*'){
-        return val1 * val2;
-    }else if(operator === '/'){
-        return val1 / val2;
-    }
-}
-
-// 연산버튼 클릭 시
-function operatorBtnHandler(){
-    const operatorBtns= document.querySelectorAll('.operator');
-    operatorBtns.forEach(operatorBtn =>{
-        
-        operatorBtn.addEventListener('click', (evt) =>{
-            const display = document.querySelector('.display');
-            let displayVal = Number(display.value);
-
-            if(evt.target.innerText === '='){
-                display.value = calculate(currentOp, currentVal, displayVal);
-                currentOp='';
-                return;
-            }
-            if(currentOp === ''){
-                currentVal = Number(display.value);
-            }else{
-                currentVal =calculate(currentOp, currentVal, displayVal);
-            }
-            display.value='';
-            currentOp=evt.target.innerText;
-        })
-    })
-}
 
 
-function operatorBtnHandler(){
-    const operatorBtns=document.querySelectorAll('.operator');
-}
-
-
-function clearBtnHandler(){
-    const clearBtn = document.querySelector('.clear');
-}
+  digitBtnHandler();
