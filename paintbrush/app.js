7e755dde3763848186db11dbbfb0dae12f5e0562
효효 <99hyolyn@gmail.com>
2021-09-09 23:35:09 +0900

    add mouse event & canvas getContext

--- paintbrush/app.js
+++ paintbrush/app.js
@@ -1,5 +1,32 @@
-const convas = document.getElementById("jsCanvas");
+const canvas = document.getElementById("jsCanvas");
+const ctx =canvas.getContext("2d");
+let painting=false;
 
 function onMouseMove(event){
     const x=event.offsetX;
+    const y=event.offsetY;
+}
+
+function stopPainting(){
+    painting=false;
+}
+
+function onMouseDown(event){
+   painting=true;
+    
+}
+
+function onMouseUp(event){
+    stopPainting();
+}
+
+function onMouseLeave(){
+    painting=false;
+}
+
+if(canvas){
+    canvas.addEventListener("mousemove", onMouseMove);
+    canvas.addEventListener("mousedown",onMouseDown);
+    canvas.addEventListener("mouseup",onMouseUp);
+    canvas.addEventListener("mouseleave",stopPainting);
 }
